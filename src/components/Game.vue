d6ca680f4c2725f6ba66892733671bbb9e1abc95
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-10-13 23:30:28 +0300

    ok

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,36 +4,37 @@
 
 <script setup lang="ts">
 
-// import kaboom from 'kaboom'; 
+import kaboom from 'kaboom'; 
  
-// const k = kaboom({ global: true, }); 
+const k = kaboom({ global: true, }); 
  
 // Загрузка спрайтов 
-// k.loadSprite('player', 'https://i.imgur.com/gO7Tqxn.png'); 
-// k.loadSprite('background', ''); 
+k.loadRoot('/')
+k.loadSprite('player', ''); 
+k.loadSprite('background', './map/level_two.png'); 
 
 
 // Создание игрового объекта (персонажа) 
-// const player = k.add([ 
-//   k.sprite('player'), 
-//   k.pos(100, 100), 
-//   k.area(), 
-//   k.body(), 
-// ]); 
+const player = k.add([ 
+  k.sprite('player'), 
+  k.pos(100, 100), 
+  k.area(), 
+  k.body(), 
+]); 
  
 // Управление игроком 
-// k.onKeyDown('left', () => { 
-//   player.move(-100, 0); 
-// }); 
-// k.onKeyDown('right', () => { 
-//   player.move(100, 0); 
-// }); 
-// k.onKeyDown('up', () => { 
-//   player.move(0, -100); 
-// }); 
-// k.onKeyDown('down', () => { 
-//   player.move(0, 100); 
-// }); 
+k.onKeyDown('left', () => { 
+  player.move(-100, 0); 
+}); 
+k.onKeyDown('right', () => { 
+  player.move(100, 0); 
+}); 
+k.onKeyDown('up', () => { 
+  player.move(0, -100); 
+}); 
+k.onKeyDown('down', () => { 
+  player.move(0, 100); 
+}); 
  
 
 </script> 
