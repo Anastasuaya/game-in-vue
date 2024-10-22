73b5894c573cb2e163838d09e917d6876ecb0a24
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-10-22 20:53:23 +0300

    like

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,15 @@
 <template>
 
-<div>
-    <!-- <GameModal
-      :isVisible="isModalVisible"
-      @update:isVisible="isModalVisible = $event"
-      @startGame="handleStartGame"
-    /> -->
-
-    <div ref="gameContainer" v-if="gameStarted" style="position: relative; width: 100%; height: 400px;">
-      <!-- Здесь будет контент игры, созданного с использованием Kaboom.js -->
-    </div>
-  </div>
 
 
-<luxInFine></luxInFine>
-
+<player></player>
 
 </template>
 
 <script setup lang="ts">
-import luxInFine from './components/Player.vue';
-import { ref, onMounted, onBeforeUnmount, defineComponent } from 'vue';
-import GameModal from './components/GameModal.vue';
-
-import kaboom from 'kaboom';
-
-const gameStarted = ref(false);
-const gameContainer = ref<HTMLDivElement | null>(null);
-let k: any;
-const isModalVisible = ref(false);
-
-
-// Открываем модальное окно при монтировании компонента
-onMounted(() => {
-  isModalVisible.value = true; // Устанавливаем состояние видимости в true
-});
-
-const handleStartGame = () => {
-  console.log('Игра начата!'); // Логика для начала игры
-  isModalVisible.value = false; // Закрываем модальное окно, когда игра начата
-};
-
-// Функция для инициализации Kaboom
-const startGame = () => {
-  if (!gameContainer.value) return;
-
-  // Создание экземпляра Kaboom
-  k = kaboom({
-    global: true,
-    // container: gameContainer.value, // Привязываем Kaboom к нашему контейнеру
-    width: 640, // ширина канваса
-    height: 480, // высота канваса
-    scale: 2, // масштабирование
-  });
-
-  // Пример добавления фона (можете заменить на свою графику)
-  k.add([
-    k.sprite('background', { // предполагаем наличие спрайта
-      url: 'https://i.imgur.com/0Z3hKib.png'
-    }),
-    k.pos(320, 240), // центр холста
-    k.scale(1), // масштаб
-  ]);
-};
-
-onBeforeUnmount(() => {
-  // Если Kaboom был инициализирован, отключаем его
-  if (k) {
-    k.destroy(); 
-  }
-});
-
-onMounted(() => {
-  // Вызываем инициализацию для первой загрузки, если нужно
-});
-
+import player from './components/player.vue';
 </script>
 
 <style scoped>
-/* Стили для основного приложения */
-button {
-  padding: 10px 20px;
-  font-size: 16px;
-}
+
 </style>
--- src/components/Player.vue
+++ src/components/Player.vue
@@ -2,98 +2,49 @@
 
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import kaboom from 'kaboom';
-import { ref, onMounted } from 'vue';
 
 const k = kaboom({background: [0,0,0]})
 
 //Загрузка спрайтов
-//Спрайты персонажа спереди
-k.loadSprite('cat', './sprites/cat_sprite.png',{
-  sliceX: 18,
-  sliceY: 9,
+k.loadSprite('cat', './sprites/cat_sprite.png', {
+  sliceX: 4,
+  sliceY: 6,
   anims: {
-    idleSitdown: { from: 0, to: 3, loop: false },
-    idleForward: { from: 8, to: 11, loop: false },
-    // idleSideL: {from: 8, to: 11, loop: true },
-    // idleBack: {from: 12, to: 15, loop: true },
-    // idleSideR: {from: 16, to: 19, loop: true },
-    // jump: {from: 20, to: 27, loop: false }
+      idleSitDown: {from: 0, to: 3, loop: false },
+      idleForward: {from: 4, to: 7, loop: true  },
+      idleSideL: {from: 8, to: 11, loop: true  },
+      idleBack: {from: 12, to: 15, loop: true  },
+      idleSideR: {from: 16, to: 19, loop: true  },
+      jump: {from: 20, to: 23, loop: false },
   }
-} ) 
+})
 
-
-//   sliceX: 8,
-//   sliceY: 6,
-//   anims: {
-//     // idleSitdown: { from: 0, to: 3, loop: false },
-//     idleForward: { from: 4, to: 7, loop: true },
-//     idleSideL: {from: 8, to: 11, loop: true },
-//     idleBack: {from: 12, to: 15, loop: true },
-//     idleSideR: {from: 16, to: 19, loop: true },
-//     jump: {from: 20, to: 27, loop: false }
-//   }
-// }) 
-
-
-//Карта 1 уровень
-// k.loadSprite('floor', './map/level_one_floor.png')
-// k.loadSprite('wall', './map/level_one_wall.png')
-// k.loadSprite('ladder', './map/level_one_ladder.png')
-
-//Создание игрового персонажа
 const cat = k.add([
-  k.sprite('cat', {anim: 'idleForward'}),
-  k.pos(100, 100),
+  k.sprite('cat'),
+  k.pos(100,100),
+  k.scale(2),
   k.body(),
-  k.area(),
-  k.scale(2)
+  k.area()
 ])
-// Начальная анимация 
-// let currentAnim = 'idleForward'
 
-// Управление игроком
-k.onKeyPress('left', () => {
-  cat.move(-100, 0);
-    cat.play('idleSideL');
-});
-
-k.onKeyPress('right', () => {
-  cat.move(100, 0);
-    cat.play('idleSideR');
-});
-
-k.onKeyPress('up', () => {
-  cat.move(0, -100);
-    cat.play('idleBack');
-});
-
-k.onKeyPress('down', () => {
-  cat.move(0, 100);
-    cat.play('idleForward');
-});
-
-// Обработка отпускания клавиш
-// k.onKeyRelease('left', () => {
-//   cat.play('idleForward');
-//   currentAnim = 'idleForward';
-// });
-
-// k.onKeyRelease('right', () => {
-//   cat.play('idleForward');
-//   currentAnim = 'idleForward';
-// });
-
-// k.onKeyRelease('up', () => {
-//   cat.play('idleForward');
-//   currentAnim = 'idleForward';
-// });
-
-// k.onKeyRelease('down', () => {
-//   cat.play('idleForward');
-//   currentAnim = 'idleForward';
-// });
+k.onKeyDown("left", () => {
+    cat.move(-100, 0)
+    cat.play('idleSideL')
+})
+k.onKeyDown("up", () => {
+    cat.move(0, -100)
+    cat.play('idleBack')
+})
+k.onKeyDown("right", () => {
+    cat.move(100, 0)
+    cat.play('idleSideR')
+})
+k.onKeyDown("down", () => {
+    cat.move(0, 100)
+    cat.play('idleForward')
+})
 </script>
 
 <style scoped>
