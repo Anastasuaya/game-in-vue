b8ae5766743ba1f6afae8cf2b7d491894f968de6
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-11-21 18:42:39 +0300

    wOw

---
+++ src/objects/objects.ts
@@ -0,0 +1,114 @@
+import { KaboomCtx } from 'kaboom';
+export const getTiles = (k:KaboomCtx)=>{
+
+
+
+k.loadSpriteAtlas('/objects/Objects_blood.png', {
+  'kid': {
+      'x': 1,
+      'y': 1,
+      'width': 25,
+      'height': 55,
+      // sliceX: 1,
+      // sliceY: 1
+  },
+  'skull': {
+      'x': 5,
+      'y': 85,
+      'width': 26,
+      'height': 25,
+
+  },
+})
+// --- ТАЙЛЫ ДЛЯ КАРТЫ ---
+k.loadSpriteAtlas('/map/base_tileset.png', {
+  'ladder': {
+     'x': 290,
+     'y': 3,
+     'width': 96,
+     'height': 96, 
+  //    sliceX: 12,
+  //    sliceY: 9
+  },
+  'ladder_lvlTwo': {
+     'x': 290,
+     'y': 95,
+     'width': 96,
+     'height': 96, 
+  //    sliceX: 12,
+  //    sliceY: 9
+  },
+  'floor': {
+     'x': 9,
+     'y': 3,
+     'width': 288,
+     'height': 96, 
+     sliceX: 9,
+     sliceY: 3,
+     
+  },
+  'floor_stone': {
+      'x': 25,
+      'y': 135,
+      'width': 200,
+     'height': 200, 
+     sliceX: 4,
+     sliceY: 6,
+  },
+  'wall-top': {
+     'x': 60,
+     'y': 194,
+     'width': 32,
+     'height': 32, 
+  },
+  'wall-topLeft': {
+      'x': 95,
+     'y': 194,
+     'width': 32,
+     'height': 32, 
+  },
+  'wall-topRight': {
+      'x': 5,
+     'y': 194,
+     'width': 32,
+     'height': 32, 
+  },
+  'wall-Left': {
+      'x': 5,
+     'y': 200,
+     'width': 32,
+     'height': 32, 
+  },
+  'wall-Right': {
+      'x': 96,
+     'y': 209,
+     'width': 32,
+     'height': 32, 
+  },
+  'wall-Bottom': {
+      'x': 25,
+     'y': 290,
+     'width': 64,
+     'height': 32, 
+  },
+  'wall-BotR': {
+      'x': 100,
+     'y': 290,
+     'width': 32,
+     'height': 32, 
+  },
+  'wall-BotL': {
+      'x': 1,
+     'y': 290,
+     'width': 32,
+     'height': 32, 
+  },
+  'obstacle': {
+      'x': 200,
+      'y': 240,
+      'width': 118,
+      'height': 32, 
+  },
+
+})
+}
---
+++ src/sprites/sprites.ts
@@ -0,0 +1,57 @@
+import { KaboomCtx } from 'kaboom';
+export const getSprites = (k:KaboomCtx)=> {
+
+  k.loadSprite('cat', '/sprites/cat_sprite.png', {
+    sliceX: 4,
+    sliceY: 9,
+    anims: {
+        idle: {from: 0, to: 3, loop: false },
+        Forward: {from: 4, to: 7, loop: true  },
+        SideL: {from: 8, to: 11, loop: true  },
+        Back: {from: 12, to: 15, loop: true  },
+        SideR: {from: 16, to: 19, loop: true  },
+        fall: {from: 20, to: 23, loop: false},
+        idleBack: { from: 24, to: 27, loop: false},
+        idleR: {from: 28, to: 31, loop: false },
+        idleL: {from: 32, to: 35, loop: false },
+  
+    }
+  })
+  
+  // --- НПС ---
+  k.loadSprite('fox', '/sprites/Fox.png', {
+      sliceX: 14,
+      sliceY: 7,
+      anims: {
+          idle: {from: 0, to: 4, loop: false},
+          look: {from: 5, to: 18, loop: true},
+          run: { from: 19 , to: 26 , loop: true},
+      }
+  })
+  
+  k.loadSprite('bat','/sprites/Bat.png', {
+      sliceX: 5,
+      sliceY: 3,
+      anims: {
+          walk: {from: 5, to: 9, loop: true }
+      }
+  })
+  
+  k.loadSprite('cobra', '/sprites/Cobra.png', {
+      sliceX: 8,
+      sliceY: 5,
+      anims: {
+          idle: {from: 0, to: 7, loop: true },
+          walk: {from: 8, to: 15, loop: true}
+      }
+  })
+  
+  k.loadSprite('dragon', '/sprites/Dragon.png', {
+      sliceX: 8,
+      sliceY: 12,
+      anims: {
+          idle: { from: 0, to: 6, loop: true},
+          walk: {from: 8, to: 14, loop: true},
+      }
+  })
+}
